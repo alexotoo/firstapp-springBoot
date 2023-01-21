378599af616c2ff4f953226ae56d6a480eb9e13b
Alex Otoo <alexotoous@outlook.com>
2023-01-21 11:55:15 -0500

    delete items implementation done

--- src/main/java/com/alexotoodev/firstapp/todo/TodoContoller.java
+++ src/main/java/com/alexotoodev/firstapp/todo/TodoContoller.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.validation.Valid;
@@ -50,9 +51,18 @@ public class TodoContoller {
             return "todo";
 
         }
-        
+
         String username =(String)model.get("name");
         todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+       
+
+       return "redirect:list-todos";
+    }
+
+     @RequestMapping("delete-todo")
+    public String deleteTodo(@RequestParam int id){
+
+        todoService.deleteById(id);
        
 
        return "redirect:list-todos";
--- src/main/java/com/alexotoodev/firstapp/todo/TodoService.java
+++ src/main/java/com/alexotoodev/firstapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.alexotoodev.firstapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,12 @@ public List<Todo> findTodoByUsername(String username){
 public void addTodo(String username, String description, LocalDate targetDate, boolean done){
     Todo todo = new Todo(++todoIdCounter, username, description, targetDate, done);
     todos.add(todo);
+
+}
+
+public void deleteById(int id){
+    Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+    todos.removeIf(predicate);
 
 }
 
