9ed196238e9056306f6d083ca66105b9ba6396b4
Alex Otoo <alexotoous@outlook.com>
2023-01-27 13:58:23 -0500

    h2 database connection config done

--- src/main/java/com/alexotoodev/firstapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/alexotoodev/firstapp/security/SpringSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.alexotoodev.firstapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -42,7 +46,24 @@ public class SpringSecurityConfiguration {
 
 
     @Bean
-    public PasswordEncoder passwordEncoder() {  return  new BCryptPasswordEncoder(); }        
+    public PasswordEncoder passwordEncoder() {  return  new BCryptPasswordEncoder(); }  
+    
+    
+
+    //configuration to be able to access h2 database
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(
+            auth -> auth.anyRequest().authenticated());
+            http.formLogin(withDefaults());
+
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+
+
+        return http.build();
+
+    }
 
 
 
